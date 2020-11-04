b6b53313d3cd497cb4eb843e455f6f9dc4655b38
Gordon Gabbay <GGabbay@Gordons-NEW-MacBook-Air.local>
2020-11-03 19:34:37 -0500

    Added picture slide

--- js/about.js
+++ js/about.js
@@ -1,3 +1,12 @@
-document.onload(e => function(){
-    alert("Hello world!");
+var slides = document.getElementsByClassName("slide");
+var gallery = document.getElementById("gallery")
+let currentSlide = 1;
+gallery.addEventListener("click", function(){
+    currentSlide = (currentSlide >= slides.length) ? 1 : currentSlide + 1;
+    slides[currentSlide - 1].style.display = "block";
+    for (let i=1; i <= slides.length; i++){
+        if (i != currentSlide) {
+            slides[i - 1].style.display = "none"
+        }
+    }
 })
